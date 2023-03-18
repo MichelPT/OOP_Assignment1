504612d610ad6b43fe6be11d3bebdd2348b049d6
MichelPT <michelpiercetahya@gmail.com>
2023-03-19 01:38:11 +0700

    added interfaces

---
+++ src/oop_assignment1/Interfaces/CalculateFlatField.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop_assignment1.Interfaces;
+
+/**
+ *
+ * @author ASUS
+ */
+public class CalculateFlatField {
+    
+}
---
+++ src/oop_assignment1/Interfaces/CalculateSpace.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop_assignment1.Interfaces;
+
+/**
+ *
+ * @author ASUS
+ */
+public interface CalculateSpace {
+    public double GetSurfaceArea();
+    public double GetVolume();
+}
